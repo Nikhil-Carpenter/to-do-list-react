3d1271cac6ee02d85465b4223c041a261043cce1
Nikhil-Carpenter <nikhilcrpntr222@gmail.com>
2022-10-13 12:47:23 +0530

    opacity added

--- src/components/Listitem.js
+++ src/components/Listitem.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Listitem = ({todo,onComplete,onDelete}) => {
   return (
-    <li key={todo.id} className='--text-base  --flex-between --mb --d-flex'>
+    <li key={todo.id} className={todo.isDone?'--text-base --rd-4 add-opacity --p --flex-between --mb --d-flex':'--text-base  --flex-between --mb --d-flex --rd-4 --p'}>
       <p className={todo.isDone?"--mr-auto --w100 isdone":"--w100 --pr --mr-auto"}>{todo.title}</p>
       <button onClick={onComplete.bind(null,todo.id)} className='--btn  --btn-success'>done</button>
       <button onClick={onDelete.bind(null,todo.id)} className='--btn --ml --btn-danger'>delete</button>
